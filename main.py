6b075de069f6a90b56e3eb4e1b9790e9540c0ba5
pikuch <55052790+pikuch@users.noreply.github.com>
2020-11-10 13:38:35 +0100

    little refactoring

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 # AOC20 main
 
 
-def run_day(n):
-    module_name = f"Day{n:02d}"
+def run_day(day):
+    module_name = f"Day{day:02d}"
     try:
         module = __import__(module_name)
+        module.run()
     except ModuleNotFoundError:
         print(f"Can't find {module_name}")
         exit(404)
-    module.run()
 
 
 def create_all_files():
-    for i in range(1, 26):
-        f_name = f"Day{i:02d}"
+    for day in range(1, 26):
+        f_name = f"Day{day:02d}"
         with open(f_name + ".py", "w") as f:
-            f.write(f"# AOC20 day {i:02d}\n\n\n")
+            f.write(f"# AOC20 day {day:02d}\n\n\n")
             f.write("def load_data(f_name):\n")
             f.write("    with open(f_name, \"r\") as f:\n")
             f.write("        data_read = f.read()\n")
             f.write("    return data_read\n\n\n")
             f.write("def run():\n")
-            f.write(f"    data = load_data(\"Day{i:02d}.txt\")\n\n")
+            f.write(f"    data = load_data(\"Day{day:02d}.txt\")\n\n")
 
         with open(f_name + ".txt", "w") as f:
             f.write("")
