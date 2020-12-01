34715fbd6d4789dd33da9574214bbeec9b9dfeb4
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-01 06:14:23 +0100

    first version

--- Day01.py
+++ Day01.py
@@ -7,6 +7,35 @@ def load_data(f_name):
     return data_read
 
 
+def to_numbers(data):
+    rows = data.split("\n")
+    return list(map(int, rows))
+
+
+def get_multiplied(numbers):
+    seen = set()
+    for n in numbers:
+        if 2020-n in seen:
+            return n*(2020-n)
+        else:
+            seen.add(n)
+    return None
+
+
+def get_multiplied3(numbers):
+    seen = set()
+    for n in numbers:
+        seen.add(n)
+    for x in numbers:
+        for y in numbers:
+            if 2020-x-y in seen:
+                return x*y*(2020-x-y)
+
+
 def run():
     data = load_data("Day01.txt")
-
+    numbers = to_numbers(data)
+    multiplied = get_multiplied(numbers)
+    print(multiplied)
+    multiplied3 = get_multiplied3(numbers)
+    print(multiplied3)
