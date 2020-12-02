71561dbaa85f50962ee2ac9486439dd879fb64d5
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-02 07:03:35 +0100

    a good class is enough

--- Day02.py
+++ Day02.py
@@ -1,6 +1,4 @@
 # AOC20 day 02
-from collections import namedtuple
-from functools import reduce
 
 
 def load_data(f_name):
@@ -9,44 +7,22 @@ def load_data(f_name):
     return data_read
 
 
-def parse_entries(data):
-    lines = data.split("\n")
-    entry = namedtuple("Entry", ["low", "high", "letter", "password"])
-    entries = []
-    for line in lines:
-        rule, password = line.split(":")
-        low_high, letter = rule.split()
-        low, high = map(int, low_high.split("-"))
-        entries.append(entry(int(low), int(high), letter, password.strip()))
-    return entries
+class PasswordEntry:
+    def __init__(self, description):
+        items = [item.strip() for item in description.split(" ")]
+        self.index1, self.index2 = tuple(map(int, items[0].split("-")))
+        self.letter = items[1][0]
+        self.password = items[2]
 
+    def is_valid_old(self):
+        return self.index1 <= self.password.count(self.letter) <= self.index2
 
-def is_valid_password(entry):
-    return entry.low <= entry.password.count(entry.letter) <= entry.high
-
-
-def count_valid_passwords(entries):
-    counter = 0
-    for entry in entries:
-        if is_valid_password(entry):
-            counter += 1
-    return counter
-
-
-def is_valid_password2(entry):
-    return (entry.password[entry.low-1] == entry.letter) + (entry.password[entry.high-1] == entry.letter) == 1
-
-
-def count_valid_passwords2(entries):
-    counter = 0
-    for entry in entries:
-        if is_valid_password2(entry):
-            counter += 1
-    return counter
+    def is_valid_new(self):
+        return (self.password[self.index1 - 1] == self.letter) != (self.password[self.index2 - 1] == self.letter)
 
 
 def run():
     data = load_data("Day02.txt")
-    entries = parse_entries(data)
-    print(count_valid_passwords(entries))  # 548
-    print(count_valid_passwords2(entries))
+    entries = [PasswordEntry(line) for line in data.split("\n")]
+    print(sum(entry.is_valid_old() for entry in entries))
+    print(sum(entry.is_valid_new() for entry in entries))
