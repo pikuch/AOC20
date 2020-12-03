02ab19bfd7c24f07d12b24609cdcb5eb0014aeb6
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-03 06:11:54 +0100

    working solution

--- Day03.py
+++ Day03.py
@@ -7,6 +7,26 @@ def load_data(f_name):
     return data_read
 
 
+def count_collisions(dc, dr, rows):
+    row = 0
+    col = 0
+    collisions = 0
+    while row < len(rows):
+        if rows[row][col] == "#":
+            collisions += 1
+        row += dr
+        col = (col + dc) % len(rows[0])
+    return collisions
+
+
 def run():
     data = load_data("Day03.txt")
-
+    rows = data.split("\n")
+    collisions = count_collisions(3, 1, rows)
+    print(collisions)
+    c2 = count_collisions(1, 1, rows) *\
+         count_collisions(3, 1, rows) *\
+         count_collisions(5, 1, rows) *\
+         count_collisions(7, 1, rows) *\
+         count_collisions(1, 2, rows)
+    print(c2)
