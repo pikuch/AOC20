ed9d473977cf66ae907b6b4d7fe0915fea39c8f7
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-06 06:20:31 +0100

    working version

--- Day06.py
+++ Day06.py
@@ -1,4 +1,5 @@
 # AOC20 day 06
+from collections import defaultdict
 
 
 def load_data(f_name):
@@ -7,6 +8,42 @@ def load_data(f_name):
     return data_read
 
 
+def get_answers(data):
+    answers = []
+    current = []
+    lines = data.split("\n")
+    for line in lines:
+        if line == "":
+            answers.append(current)
+            current = []
+        else:
+            current.append(line)
+    answers.append(current)
+    return answers
+
+
 def run():
     data = load_data("Day06.txt")
+    answers = get_answers(data)
 
+    different_counter = 0
+    for group in answers:
+        different = set()
+        for person in group:
+            for c in person:
+                different.add(c)
+        different_counter += len(different)
+
+    print(different_counter)
+
+    same_counter = 0
+    for group in answers:
+        counts = defaultdict(lambda: 0)
+        for person in group:
+            for c in person:
+                counts[c] += 1
+        for ans in counts.values():
+            if ans == len(group):
+                same_counter += 1
+
+    print(same_counter)
