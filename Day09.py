97fddce9c1b6ebfdcd1f6b5f93b9e63975eb1579
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-09 06:31:21 +0100

    slightly faster series searching function

--- Day09.py
+++ Day09.py
@@ -25,14 +25,12 @@ def find_invalid(signal, preamble):
 def find_series_limits(signal, key):
     for start_index in range(len(signal)):
         the_sum = 0
-        sum_list = []
         length = 0
         while the_sum < key:
             the_sum += signal[start_index+length]
-            sum_list.append(signal[start_index+length])
             length += 1
         if the_sum == key:
-            return sorted(sum_list)
+            return sorted(signal[start_index:start_index+length])
     return None
 
 
