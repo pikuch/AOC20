ee636debca66f807536ba28c628b45d24ef82d7e
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-10 07:15:38 +0100

    paths counting function added

--- Day10.py
+++ Day10.py
@@ -15,7 +15,6 @@ def prepare_list(data):
 def get_diff_counts(joltages):
     differences = [0]*4
     for i in range(len(joltages)-1):
-        print(joltages[i+1] - joltages[i], end=" ")
         differences[joltages[i+1] - joltages[i]] += 1
     return differences
 
@@ -27,15 +26,35 @@ def get_diff_list(joltages):
     return diffs
 
 
+def count_paths(diffs):
+    segments = []
+    index = 0
+    segment_length = 0
+    while index < len(diffs):
+        if diffs[index] == 3:
+            segments.append(segment_length)
+            segment_length = 0
+        elif diffs[index] == 1:
+            segment_length += 1
+        else:
+            return None
+        index += 1
+
+    paths = 1
+    for segment in segments:  # there are only a few paths through
+        if segment == 2:
+            paths *= 2
+        elif segment == 3:
+            paths *= 4
+        elif segment == 4:
+            paths *= 7
+
+    return paths
+
+
 def run():
     data = load_data("Day10.txt")
     joltages = prepare_list(data)
     diff_counts = get_diff_counts(joltages)
     print(diff_counts[1] * diff_counts[3])
-    #print(get_diff_list())
-    print(2**3 * 4**7 * 7**10)
-
-# 3 * [1 1 3] = 2**3
-# 7 * [1 1 1 3] = 4**7
-# 10 * [1 1 1 1 3] = 7**10
-# 37024595836928
+    print(count_paths(get_diff_list(joltages)))
