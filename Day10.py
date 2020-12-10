cd2e78edf8d2efffc04c3d7e4a9a1852e75399a3
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-10 06:18:27 +0100

    part 1 done

--- Day10.py
+++ Day10.py
@@ -7,6 +7,17 @@ def load_data(f_name):
     return data_read
 
 
+def get_diff(joltages):
+    differences = [0]*4
+    for i in range(len(joltages)-1):
+        differences[joltages[i+1] - joltages[i]] += 1
+    return differences
+
+
 def run():
-    data = load_data("Day10.txt")
+    data = load_data("Day10.txt").split("\n")
+    joltages = sorted(list(map(int, data)))
+    joltages = [0] + joltages + [(joltages[-1] + 3)]
+    differences = get_diff(joltages)
+    print(differences[1]*differences[3])
 
