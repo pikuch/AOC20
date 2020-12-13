9784ed45f43e8fcf82f1d87b6858fbab184a8717
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-13 06:17:18 +0100

    test passed

--- Day13.py
+++ Day13.py
@@ -7,6 +7,21 @@ def load_data(f_name):
     return data_read
 
 
+def parse_data(data):
+    lines = data.split("\n")
+    arrival = int(lines[0])
+    busses = []
+    for element in lines[1].split(","):
+        if element != "x":
+            busses.append(int(element))
+    return arrival, busses
+
+
 def run():
-    data = load_data("Day13.txt")
+    data = load_data("Day13test0.txt")
+    arrival, busses = parse_data(data)
+
+    wait_times = [bus - arrival % bus for bus in busses]
+    print(list(wait*bus for wait, bus in zip(wait_times, busses) if wait == min(wait_times)))
+
 
