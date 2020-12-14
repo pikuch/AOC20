cce0664b6a74c17c09afb5edcc3dee2c1eee32c0
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-14 08:41:00 +0100

    reversed mask for clearness

--- Day14.py
+++ Day14.py
@@ -13,32 +13,42 @@ def read_instructions(lines):
     for line in lines:
         items = line.split(" = ")
         if items[0] == "mask":
-            program.append(items)
+            program.append((items[0], tuple(reversed(items[1]))))
         else:
             program.append([int(items[0][4:-1]), int(items[1])])
     return program
 
 
-def execute_with_mask(inst, mask, memory):
-    value = f"{inst[1]:036b}"
-    output = 0
-    for bit in range(36):
-        if mask[-bit-1] == "X":
-            output += int(value[-bit-1]) * (2**bit)
-        else:
-            output += int(mask[-bit-1]) * (2**bit)
-    memory[inst[0]] = output
-
-
-def execute(program):
+def execute(program, part):
     memory = dict()
-    mask = "X" * 36
+    mask = ["X"] * 36
     for inst in program:
         if inst[0] == "mask":
             mask = inst[1]
         else:
-            execute_with_mask(inst, mask, memory)
+            if part == 1:  # part 1: override values with mask
+                execute_with_mask(inst, mask, memory)
+            else:  # part 2: memory address decoder
+                execute_with_mask2(inst, mask, memory)
     return memory
+
+
+def execute_with_mask(inst, mask, memory):
+    value = tuple(reversed(f"{inst[1]:036b}"))
+    output = 0
+    for bit in range(36):
+        if mask[bit] == "X":
+            current = value[bit]
+        else:
+            current = mask[bit]
+        output += int(current) * (2 ** bit)
+    memory[inst[0]] = output
+
+
+def execute_with_mask2(inst, mask, memory):
+    address = tuple(reversed(f"{inst[0]:036b}"))
+    for new_address in generate_addresses(address, mask):
+        memory[new_address] = inst[1]
 
 
 def generate_addresses(address, mask):
@@ -49,36 +59,21 @@ def generate_addresses(address, mask):
         if bit == 36:
             yield value
         else:
-            if mask[-bit-1] == "0":
-                to_check.append((value + int(address[-bit-1]) * (2 ** bit), bit+1))
-            elif mask[-bit-1] == "1":
-                to_check.append((value + (2 ** bit), bit+1))
+            if mask[bit] == "0":
+                to_check.append((value + int(address[bit]) * (2 ** bit), bit + 1))
+            elif mask[bit] == "1":
+                to_check.append((value + (2 ** bit), bit + 1))
             else:
                 to_check.append((value, bit + 1))
                 to_check.append((value + (2 ** bit), bit + 1))
 
 
-def execute_with_mask2(inst, mask, memory):
-    address = f"{inst[0]:036b}"
-    for new_address in generate_addresses(address, mask):
-        memory[new_address] = inst[1]
-
-
-def execute2(program):
-    memory = dict()
-    mask = "X" * 36
-    for inst in program:
-        if inst[0] == "mask":
-            mask = inst[1]
-        else:
-            execute_with_mask2(inst, mask, memory)
-    return memory
-
-
 def run():
     data = load_data("Day14.txt")
     program = read_instructions(data.split("\n"))
-    memory = execute(program)
+    memory = execute(program, 1)
     print(sum(memory.values()))
-    memory2 = execute2(program)
-    print(sum(memory2.values()))
+    # assert sum(memory.values()) == 7817357407588
+    memory = execute(program, 2)
+    print(sum(memory.values()))
+    # assert sum(memory.values()) == 4335927555692
