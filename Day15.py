04a4ccfc646c3b5db4be50e2cdb34807a723944b
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-15 06:38:50 +0100

    test passed

--- Day15.py
+++ Day15.py
@@ -7,6 +7,23 @@ def load_data(f_name):
     return data_read
 
 
-def run():
-    data = load_data("Day15.txt")
+def generate(n, numbers):
+    last_seen = dict()
+    for i in range(len(numbers)-1):
+        last_seen[numbers[i]] = i+1
 
+    while len(numbers) < n:
+        current = numbers[-1]
+        if current in last_seen.keys():
+            numbers.append(len(numbers) - last_seen[current])
+            last_seen[current] = len(numbers)-1
+        else:
+            last_seen[current] = len(numbers)
+            numbers.append(0)
+
+
+def run():
+    data = load_data("Day15test0.txt")
+    numbers = list(map(int, data.split(",")))
+    generate(2020, numbers)
+    print(numbers[-1])
