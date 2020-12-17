85b5077c5112fa64d084a0802b3c077345434541
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-17 07:25:24 +0100

    ConwayGrid3d added

--- Day17.py
+++ Day17.py
@@ -8,6 +8,47 @@ def load_data(f_name):
     return data_read
 
 
+class ConwayGrid3d:
+    def __init__(self, data):
+        self.near_tiles = [(dx, dy, dz)
+                           for dx in range(-1, 2)
+                           for dy in range(-1, 2)
+                           for dz in range(-1, 2)
+                           if not (dx == dy == dz == 0)]
+        self.grid = set()
+        lines = data.splitlines()
+        for y in range(len(lines)):
+            for x in range(len(lines[0])):
+                if lines[y][x] == "#":
+                    self.grid.add((x, y, 0))
+
+    def neighbours(self, x, y, z):
+        n = 0
+        for dx, dy, dz in self.near_tiles:
+            if (x+dx, y+dy, z+dz) in self.grid:
+                n += 1
+        return n
+
+    def step(self):
+        considered = set()
+        changed = []
+        for x, y, z in self.grid:
+            considered.add((x, y, z))
+            for dx, dy, dz in self.near_tiles:
+                considered.add((x+dx, y+dy, z+dz))
+        for x, y, z in considered:
+            n = self.neighbours(x, y, z)
+            if (x, y, z) not in self.grid and n == 3:
+                changed.append((x, y, z, 1))
+            elif (x, y, z) in self.grid and (n < 2 or n > 3):
+                changed.append((x, y, z, 0))
+        for x, y, z, change in changed:
+            if change == 1:
+                self.grid.add((x, y, z))
+            else:
+                self.grid.discard((x, y, z))
+
+
 def load_cubes(lines):
     cubes = defaultdict(lambda: 0)
     for y in range(len(lines)):
@@ -83,6 +124,14 @@ def neighbours4(x, y, z, w, cubes):
 
 def run():
     data = load_data("Day17.txt")
+    grid = ConwayGrid3d(data)
+    for i in range(6):
+        grid.step()
+    print(len(grid.grid))
+
+    # 240 1180
+
+    exit(1)
     cubes = load_cubes(data.splitlines())
     for i in range(6):
         step(cubes)
