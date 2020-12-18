dfe87834364a5fd0821e423d878c3a730f0b9c00
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-18 07:23:28 +0100

    added a function to evaluate stack top

--- Day18.py
+++ Day18.py
@@ -8,13 +8,21 @@ def load_data(f_name):
     return data_read
 
 
-def operation(x, oper, y):
-    if oper == "+":
+def operation(x, op, y):
+    if op == "+":
         return x+y
-    elif oper == "*":
+    elif op == "*":
         return x*y
     else:
-        return ""
+        print(f"Invalid operation: {x}{op}{y}")
+        exit(1)
+
+
+def eval_stack_top(stack_top, new_item):
+    if len(stack_top):
+        stack_top.append(operation(new_item, stack_top.pop(), stack_top.pop()))
+    else:
+        stack_top.append(new_item)
 
 
 def evaluate(expr):
@@ -23,10 +31,7 @@ def evaluate(expr):
     stack = [deque()]
     for item in items:
         if item in "0123456789":
-            if len(stack[-1]):
-                stack[-1].append(operation(int(item), stack[-1].pop(), stack[-1].pop()))
-            else:
-                stack[-1].append(int(item))
+            eval_stack_top(stack[-1], int(item))
         elif item in "*+":
             stack[-1].append(item)
         elif item == "(":
@@ -34,18 +39,44 @@ def evaluate(expr):
         elif item == ")":
             ret = stack[-1].pop()
             stack.pop()
-            if len(stack[-1]):
-                stack[-1].append(operation(ret, stack[-1].pop(), stack[-1].pop()))
-            else:
-                stack[-1].append(ret)
+            eval_stack_top(stack[-1], ret)
     return stack[-1].pop()
 
 
+def eval_stack_top_plus(stack_top, new_item):
+    if len(stack_top):
+        if stack_top[-1] == "+":
+            stack_top.append(operation(new_item, stack_top.pop(), stack_top.pop()))
+        else:
+            stack_top.append(new_item)
+    else:
+        stack_top.append(new_item)
 
+
+def evaluate_plus(expr):
+    expr = expr.replace("(", "( ").replace(")", " )")
+    items = expr.split()
+    stack = [deque()]
+    for item in items:
+        if item in "0123456789":
+            eval_stack_top_plus(stack[-1], int(item))
+        elif item in "*+":
+            stack[-1].append(item)
+        elif item == "(":
+            stack.append(deque())
+        elif item == ")":
+            while len(stack[-1]) > 1:
+                stack[-1].append(operation(stack[-1].pop(), stack[-1].pop(), stack[-1].pop()))
+            ret = stack[-1].pop()
+            stack.pop()
+            eval_stack_top_plus(stack[-1], ret)
+    while len(stack[-1]) > 1:
+        stack[-1].append(operation(stack[-1].pop(), stack[-1].pop(), stack[-1].pop()))
+    return stack[-1].pop()
 
 
 def run():
     data = load_data("Day18.txt")
     expressions = data.splitlines()
-    print(sum(map(evaluate, expressions)))
-    #print(sum(map(evaluate_plus, expressions)))
+    print(sum(map(evaluate, expressions)))  # 7293529867931
+    print(sum(map(evaluate_plus, expressions)))  # 60807587180737
