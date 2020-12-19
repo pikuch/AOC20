50d44bf854918693fdfb17f0ffe78aaaa3e5745b
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-19 09:13:32 +0100

    removed Rule class

--- Day19.py
+++ Day19.py
@@ -8,14 +8,11 @@ def load_data(f_name):
     return data_read
 
 
-class Rule:
-    def __init__(self, s):
-        if s.startswith("\""):
-            self.type = "final"
-            self.value = s[1]
-        else:
-            self.type = "options"
-            self.value = list(map(lambda option: list(map(int, option.split())), s.split(" | ")))
+def parse_rule(s):
+    if s.startswith("\""):
+        return s[1]
+    else:
+        return list(map(lambda option: list(map(int, option.split())), s.split(" | ")))
 
 
 def get_splits(s, n):
@@ -37,13 +34,13 @@ class Ruleset:
         self.rules = dict()
         for line in s.splitlines():
             rule_number_string, rule_description = line.split(": ")
-            self.rules[int(rule_number_string)] = Rule(rule_description)
+            self.rules[int(rule_number_string)] = parse_rule(rule_description)
 
     def match(self, s, rule):
-        if self.rules[rule].type == "final":
-            return s == self.rules[rule].value
+        if isinstance(self.rules[rule], str):
+            return s == self.rules[rule]
         else:
-            return any(map(lambda subrules: self.match_subrules(s, subrules), self.rules[rule].value))
+            return any(map(lambda subrules: self.match_subrules(s, subrules), self.rules[rule]))
 
     def match_subrules(self, s, subrules):
         return any(map(lambda substrings: all(map(lambda sr: self.match(sr[0], sr[1]),
@@ -57,6 +54,8 @@ def parse_data(data):
 
 
 def run():
-    data = load_data("Day19test1.txt")
+    data = load_data("Day19.txt")
     rules, strings = parse_data(data)
-    print(sum(map(lambda s: rules.match(s, 0), strings)))
+    rules.match(strings[0], 0)
+    exit(1)
+    print(sum([rules.match(s, 0) for s in strings]))
