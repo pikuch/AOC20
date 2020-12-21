69247fa4a3a55c1df2b8a4d5b944675da42683bc
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-21 06:47:39 +0100

    part 1 done

--- Day19.py
+++ Day19.py
@@ -9,6 +9,11 @@ def load_data(f_name):
     return data_read
 
 
+def parse_data(data):
+    rule_data, string_data = data.split("\n\n")
+    return StateMachine(rule_data), string_data.splitlines()
+
+
 def parse_rule(s):
     if s.startswith("\""):
         return s[1]
@@ -16,62 +21,73 @@ def parse_rule(s):
         return list(map(lambda option: list(map(int, option.split())), s.split(" | ")))
 
 
-def get_splits(s, n):
-    if n == 1:
-        yield [s]
-    elif n == 2:
-        for num in range(1, len(s)):
-            yield s[:num], s[num:]
-    elif n == 3:
-        for i, j in combinations(range(1, len(s)), 2):
-            yield s[:i], s[i:j], s[j:]
-    else:
-        print("Oops!")  # not gonna happen
-        exit(1)
+class State:
+    def __init__(self, value, options):
+        self.value = value
+        self.options = options
 
 
-class Ruleset:
-    def __init__(self, s):
+class StateMachine:
+    def __init__(self, data):
         self.rules = dict()
-        for line in s.splitlines():
+        for line in data.splitlines():
             rule_number_string, rule_description = line.split(": ")
             self.rules[int(rule_number_string)] = parse_rule(rule_description)
 
-    @lru_cache()
-    def match(self, s, rule):
+        finish = State("finish", [])
+        start = State("start", [])
+        start.options = self.evaluate_states(0, finish)
+        self.states = start
+
+    def evaluate_states(self, rule, downstream):
         if isinstance(self.rules[rule], str):
-            return s == self.rules[rule]
+            return State(self.rules[rule], [downstream])
         else:
-            for subrules in self.rules[rule]:
-                if self.match_subrules(s, tuple(subrules)):
-                    return True
-            return False
-
-    @lru_cache()
-    def match_subrules(self, s, subrules):
-        for subs in get_splits(s, len(subrules)):
-            all_match = True
-            for i in range(len(subrules)):
-                if not self.match(subs[i], subrules[i]):
-                    all_match = False
-                    break
-            if all_match:
-                return True
-        return False
+            options = []
+            for alternative in self.rules[rule]:
+                link = downstream
+                for new_rule in reversed(alternative):
+                    node = State(self.evaluate_states(new_rule, link), link)
+                    link = node
+                options.append(node)
+            return [State("", options)]
 
 
-def parse_data(data):
-    rule_data, string_data = data.split("\n\n")
-    return Ruleset(rule_data), string_data.splitlines()
+# class Ruleset:
+#     def __init__(self, s):
+#         self.rules = dict()
+#         for line in s.splitlines():
+#             rule_number_string, rule_description = line.split(": ")
+#             self.rules[int(rule_number_string)] = parse_rule(rule_description)
+#
+#     @lru_cache()
+#     def match(self, s, rule):
+#         if isinstance(self.rules[rule], str):
+#             return s == self.rules[rule]
+#         else:
+#             for subrules in self.rules[rule]:
+#                 if self.match_subrules(s, tuple(subrules)):
+#                     return True
+#             return False
+#
+#     @lru_cache()
+#     def match_subrules(self, s, subrules):
+#         for subs in get_splits(s, len(subrules)):
+#             all_match = True
+#             for i in range(len(subrules)):
+#                 if not self.match(subs[i], subrules[i]):
+#                     all_match = False
+#                     break
+#             if all_match:
+#                 return True
+#         return False
 
 
 def run():
-    data = load_data("Day19.txt")
+    data = load_data("Day19test0.txt")
+    state_machine, strings = parse_data(data)
+    print(state_machine.states)
 
     # TODO: parse into a state machine and step from state to state in O(n)
 
-    rules, strings = parse_data(data)
-    for s in strings[:1]:
-        print(rules.match(strings[0], 0))
-#    exit(0)
-    print(sum([rules.match(s, 0) for s in strings]))
+    # print(sum([rules.match(s, 0) for s in strings]))
--- Day21.py
+++ Day21.py
@@ -7,6 +7,48 @@ def load_data(f_name):
     return data_read
 
 
+def parse_foods(data):
+    foods = []
+    for line in data.splitlines():
+        ingredients_part, allergens_part = line.split(" (contains ")
+        ingredients = ingredients_part.split()
+        allergens = allergens_part[:-1].split(", ")
+        foods.append([ingredients, allergens])
+    return foods
+
+
+def find_dangerous_ingredients(foods):
+    allergen_options = dict()
+    dangerous_ingredients = dict()
+    for food in foods:
+        if len(food[1]):
+            for allergen in food[1]:
+                if allergen in allergen_options:
+                    allergen_options[allergen].intersection_update(food[0])
+                else:
+                    allergen_options[allergen] = set(food[0])
+    updates = 1
+    while updates:
+        updates = 0
+        for allergen in allergen_options.keys():
+            if len(allergen_options[allergen]) == 1:
+                dangerous_ingredients[allergen] = list(allergen_options[allergen])[0]
+                for a in allergen_options.keys():
+                    allergen_options[a].discard(dangerous_ingredients[allergen])
+                updates += 1
+                break
+    return dangerous_ingredients
+
+
+def count_safe_ingredients(foods, dangerous_ingredients):
+    count = 0
+    for food in foods:
+        count += len([ingredient for ingredient in food[0] if ingredient not in dangerous_ingredients.values()])
+    return count
+
+
 def run():
     data = load_data("Day21.txt")
-
+    foods = parse_foods(data)
+    dangerous_ingredients = find_dangerous_ingredients(foods)
+    print(count_safe_ingredients(foods, dangerous_ingredients))
