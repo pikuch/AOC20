02ca27bd09e9570f606d5bb04b64211652646048
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-24 06:59:23 +0100

    working solution

--- Day24.py
+++ Day24.py
@@ -1,5 +1,4 @@
 # AOC20 day 24
-from collections import defaultdict
 
 
 def load_data(f_name):
@@ -45,17 +44,67 @@ def decode(data):
 
 
 def flip(inst, tiles):
-    r, c = 0, 0
+    x, y, z = 0, 0, 0
     for i in inst:
-        r += i[0]
-        c += i[1]
-    tiles[(r, c)] = 1 - tiles[(r, c)]
+        x += i[0]
+        y += i[1]
+        z += i[2]
+    if (x, y, z) in tiles:
+        tiles.remove((x, y, z))
+    else:
+        tiles.add((x, y, z))
+
+
+def count_neighbours(x, y, z, tiles):
+    count = 0
+    if (x - 2, y + 1, z + 1) in tiles:
+        count += 1
+    if (x + 2, y - 1, z - 1) in tiles:
+        count += 1
+    if (x + 1, y - 2, z + 1) in tiles:
+        count += 1
+    if (x - 1, y + 2, z - 1) in tiles:
+        count += 1
+    if (x + 1, y + 1, z - 2) in tiles:
+        count += 1
+    if (x - 1, y - 1, z + 2) in tiles:
+        count += 1
+    return count
+
+
+def step(tiles):
+    considered = set()
+    for x, y, z in tiles:
+        considered.add((x, y, z))
+        considered.add((x-2, y+1, z+1))
+        considered.add((x+2, y-1, z-1))
+        considered.add((x+1, y-2, z+1))
+        considered.add((x-1, y+2, z-1))
+        considered.add((x+1, y+1, z-2))
+        considered.add((x-1, y-1, z+2))
+    changes = []
+    for x, y, z in considered:
+        n = count_neighbours(x, y, z, tiles)
+        if (x, y, z) in tiles and n not in (1, 2):
+            changes.append((x, y, z, 0))
+        if (x, y, z) not in tiles and n == 2:
+            changes.append((x, y, z, 1))
+    # apply changes
+    for x, y, z, c in changes:
+        if c == 0:
+            tiles.remove((x, y, z))
+        else:
+            tiles.add((x, y, z))
 
 
 def run():
     data = load_data("Day24.txt")
     instructions = decode(data)
-    tiles = defaultdict(lambda: 0)
+    tiles = set()
     for inst in instructions:
         flip(inst, tiles)
-    print(sum(tile == 1 for tile in tiles.values()))
+    print(len(tiles))
+    for t in range(100):
+        print(f"\rstep {t}", end="")
+        step(tiles)
+    print(f"\nThere are now {len(tiles)} black tiles")
