8131d7a7002ee1098f0b91ae40fdcd7d235df8d6
pikuch <55052790+pikuch@users.noreply.github.com>
2020-12-29 21:33:56 +0100

    all done

--- Day23.py
+++ Day23.py
@@ -1,5 +1,4 @@
 # AOC20 day 23
-import numpy as np
 
 
 def load_data(f_name):
@@ -12,125 +11,55 @@ class Cup:
     def __init__(self, value):
         self.val = value
         self.next = self
-        self.prev = self
 
 
 class Cups:
     def __init__(self, elements):
-        self.length = len(elements)
-        self.current = last = Cup(elements[0])
-        for el in elements[1:]:
-            last = self.insert_after(last, Cup(el))
-        self.picked = []
+        self.items = [None] * (len(elements) + 1)  # not using the 0 index
+        for element in elements:
+            self.items[element] = Cup(element)
+        for index in range(len(elements) - 1):
+            self.items[elements[index]].next = self.items[elements[index + 1]]
+        self.items[elements[len(elements) - 1]].next = self.items[elements[0]]
+        self.current = self.items[elements[0]]
         self.destination = self.current
-
-    def insert_after(self, last_cup, new_cup):
-        new_cup.next = last_cup.next
-        new_cup.prev = last_cup
-        new_cup.prev.next = new_cup
-        new_cup.next.prev = new_cup
-        return new_cup
-
-    def pick_next(self):
-        next_cup = self.current.next
-        self.current.next = next_cup.next
-        next_cup.next.prev = self.current
-        return next_cup
+        self.picked = None
 
     def pick_3(self):
-        self.picked.clear()
-        for _ in range(3):
-            self.picked.append(self.pick_next())
+        self.picked = self.current.next
+        self.current.next = self.current.next.next.next.next
 
     def place_picked(self):
-        cursor = self.destination
-        for cup in self.picked:
-            cursor = self.insert_after(cursor, cup)
+        after = self.destination.next
+        self.destination.next = self.picked
+        self.picked.next.next.next = after
 
     def is_picked(self, n):
-        return self.picked[0].val == n or self.picked[1].val == n or self.picked[2].val == n
+        return self.picked.val == n or self.picked.next.val == n or self.picked.next.next.val == n
 
     def next_number(self, n):
-        return n - 1 if n > 1 else self.length
-
-    def cursor_to(self, n):
-        while self.current.val != n:
-            self.current = self.current.next
-
-    def destination_to(self, n):
-        while self.destination.val != n:
-            self.destination = self.destination.next
+        return n - 1 if n > 1 else len(self.items) - 1
 
     def go_to_destination(self):
-        number = self.next_number(self.current.val)
-        while self.is_picked(number):
+        number = self.current.val
+        while True:
             number = self.next_number(number)
-        self.destination_to(number)
+            if not self.is_picked(number):
+                break
+        self.destination = self.items[number]
 
     def move(self):
-        #self.show()
         self.pick_3()
-        #self.show_picked()
         self.go_to_destination()
-        #self.show_destination()
         self.place_picked()
         self.current = self.current.next
 
-    def show(self):
-        print(f"({self.current.val})", end="")
-        cursor = self.current.next
-        while cursor != self.current:
-            print(f" {cursor.val} ", end="")
-            cursor = cursor.next
-        print()
-
-    def show_picked(self):
-        for el in self.picked:
-            print(f"{el.val} ", end="")
-        print()
-
-    def show_destination(self):
-        print(self.destination.val)
-
     def show_after_1(self):
-        self.cursor_to(1)
-        cursor = self.current.next
-        while cursor != self.current:
+        cursor = self.items[1].next
+        while cursor.val != 1:
             print(f"{cursor.val}", end="")
             cursor = cursor.next
         print()
-
-
-class CupGame:
-    def __init__(self, cups):
-        self.cups = np.array(list(map(int, cups)), dtype=np.int)
-        self.current = 0
-        self.picked = []
-        self.destination = 0
-
-    def pick_3(self):
-        if self.current + 3 < self.cups.size:
-            self.picked = list(self.cups[self.current + 1:self.current + 4])
-        else:
-            self.picked = list(self.cups[self.current+1:])
-            self.picked += list(self.cups[:3-len(self.picked)])
-
-    def find_destination(self):
-        destination_value = self.cups[self.current]
-        while True:
-            destination_value = destination_value - 1 if destination_value > 1 else self.cups.size
-            if destination_value not in self.picked:
-                break
-        self.destination = np.where(self.cups == destination_value)
-
-    def place_picked(self):
-        pass
-
-    def move(self):
-        self.pick_3()
-        self.find_destination()
-        self.place_picked()
-        self.current = (self.current + 1) % self.cups.size
 
 
 def run():
@@ -138,10 +67,13 @@ def run():
     cups = Cups(list(map(int, data)))
     for i in range(100):
         cups.move()
-    cups.show_after_1()  # 38756249
+    cups.show_after_1()
 
-    cupgame = CupGame(data)
-    for i in range(100):
-        cupgame.move()
-
-    # cups = Cups(list(map(int, data)) + list(range(len(data)+1, 10**6 + 1)))
+    cups = Cups(list(map(int, data)) + list(range(len(data)+1, 10**6 + 1)))
+    for i in range(10**7):
+        cups.move()
+        if i % 1000 == 0:
+            print(f"\r{100*i/(10**7):.1f}%", end="")
+    print(" done")
+    first, second = cups.items[1].next.val, cups.items[1].next.next.val
+    print(f"{first} * {second} = {first * second}")
